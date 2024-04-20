1c95cabda84f6b567232cde44b0d0a8c30c672b7
SunitCodes <sunitdasclassx@gmail.com>
2024-04-20 21:02:45 +0530

    files added

---
+++ contracts/Hello.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+//software package data exchange - this line says who can use this code
+//will it be open souce or only for me
+
+pragma solidity ^0.8.25;
+//You need to specify which compiler is going to compile this code
+
+contract Hello {
+    string public welcome = "Hello World";
+
+}
+
